a4262b1edd0fde3a20c2707647b9154b037f3639
rebeccamorandini <rebecca.santos16@etec.sp.gov.br>
2024-03-05 11:44:20 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Rebecca Morandini Dos Santos</Text>
+      <Text>Pedro Augusto Salla</Text>
+      <Text>05/03/2024</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
